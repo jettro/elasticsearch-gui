963287528e26cb8d891f4c1a592841dfa2fef5d9
jettro <jettro@gridshore.nl>
2013-02-13 21:49:17 +0100

    Added a better highlighting using a directive

--- js/app.js
+++ js/app.js
@@ -2,11 +2,27 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'elasticjs.service','myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: DashboardCtrl});
-    $routeProvider.when('/node/:nodeId', {templateUrl: 'partials/node.html', controller: NodeInfoCtrl});
-    $routeProvider.when('/stats', {templateUrl: 'partials/stats.html', controller: StatsCtrl});
-    $routeProvider.when('/query', {templateUrl: 'partials/query.html', controller: QueryCtrl});
-    $routeProvider.otherwise({redirectTo: '/dashboard'});
-  }]);
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'elasticjs.service', 'myApp.directives']).
+        config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: DashboardCtrl});
+            $routeProvider.when('/node/:nodeId', {templateUrl: 'partials/node.html', controller: NodeInfoCtrl});
+            $routeProvider.when('/stats', {templateUrl: 'partials/stats.html', controller: StatsCtrl});
+            $routeProvider.when('/query', {templateUrl: 'partials/query.html', controller: QueryCtrl});
+            $routeProvider.otherwise({redirectTo: '/dashboard'});
+        }]).
+        directive('navbar', ['$location', function ($location) {
+            return {
+                restrict: 'EA',
+                transclude: true,
+                scope: {heading: '@'},
+                controller: 'NavbarCtrl',
+                templateUrl: 'templates/navbar/navbar.html',
+                replace: true,
+                link: function ($scope, $element, $attrs, navbarCtrl) {
+                    $scope.$location = $location;
+                    $scope.$watch('$location.path()', function (locationPath) {
+                        navbarCtrl.selectByUrl(locationPath)
+                    });
+                }
+            }
+        }]);
--- js/controllers.js
+++ js/controllers.js
@@ -183,3 +183,26 @@ function QueryCtrl($scope, $http, ejsResource) {
 
     $scope.resetQuery();
 }
+
+function NavbarCtrl($scope) {
+    var items = $scope.items = [
+        {title: 'Home', link: 'dashboard'},
+        {title: 'Queries', link: 'query'},
+        {title: 'Statistics', link: 'stats'}
+    ];
+
+    this.select = $scope.select = function (item) {
+        angular.forEach(items, function (item) {
+            item.selected = false;
+        });
+        item.selected = true;
+    };
+
+    this.selectByUrl = function (url) {
+        angular.forEach(items, function (item) {
+            if ('/' + item.link === url) {
+                $scope.select(item);
+            }
+        });
+    };
+}

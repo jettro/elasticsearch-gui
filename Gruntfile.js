db2c97e7ba9302b59c785506e616b7e6dd3f6d78
Jettro Coenradie <jettrocoenradie@Jettros-MacBook-Pro.local>
2016-05-31 09:44:22 +0200

    Upgraded versions in bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
         watch: {
             js: {
                 files: '<%= jshint.all %>',
-                tasks: ['concat']
+                tasks: ['combine','rsync:deploy']
             },
             sass: {
                 files: ['sass/*.scss'],
